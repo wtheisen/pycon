f1fea9a69dfa031a4aa4fc01292a915d8f69407a
wtheisen <wtheisen@nd.edu>
2016-07-24 17:32:37 -0400

    Changes to allow the manager to send notifications

--- symposion/reviews/context_processors.py
+++ symposion/reviews/context_processors.py
@@ -3,9 +3,13 @@ from symposion.proposals.models import ProposalSection
 
 def reviews(request):
     sections = []
+    manager = []
     for section in ProposalSection.objects.select_related('section'):
         if request.user.has_perm("reviews.can_review_%s" % section.section.slug):
             sections.append(section)
+            if request.user.has_perm("reviews.can_manage_%s" % section.section.slug):
+                manager.append(True)
+    temp = zip(sections, manager)
     return {
-        "review_sections": sections,
+        "review_sections": temp,
     }
