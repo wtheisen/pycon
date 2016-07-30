8cbcf894146a0db7f64f8393f1d5e40c0f9a6010
Bill Theisen <bilbo991@wtheisen.com>
2016-07-29 20:30:35 -0400

    Changed back to lists and tuples from dictionaries for future expandability

--- symposion/reviews/context_processors.py
+++ symposion/reviews/context_processors.py
@@ -2,15 +2,17 @@ from symposion.proposals.models import ProposalSection
 
 
 def reviews(request):
-    sections = {}
-    #manager = []
+    sections = []
     for section in ProposalSection.objects.select_related('section'):
         if request.user.has_perm("reviews.can_review_%s" % section.section.slug):
-            sections[section] = False
+            info = []
+            info.append(section)
             if request.user.has_perm("reviews.can_manage_%s" % section.section.slug):
-                sections[section] = True
-    #temp = zip(sections, manager)
-    print 'hello'
+                info.append(True)
+            else:
+                info.append(False)
+            sections.append(info)
+    print sections
     return {
         "review_sections": sections,
     }
